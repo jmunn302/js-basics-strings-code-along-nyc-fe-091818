54493da5dd5a81a851f429aa4c40416ab40a4dc6
Julie Munn <jmunn302@gmail.com>
2018-10-17 00:48:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 let currentUser = 'Julie'
 let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
 let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() +'!'
-let shortGreeting = 'Welcome, ' + currentUser +'!'
+let shortGreeting = 'Welcome, ' + currentUser.charAt(0) +'!'
