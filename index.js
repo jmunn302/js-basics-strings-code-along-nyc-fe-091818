babe8e8a30410e30d991e8c762b66482f813f0eb
Julie Munn <jmunn302@gmail.com>
2018-10-17 00:44:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 let currentUser = 'Julie'
-let welcomeMessage = 'Welcome to Flatbook, ' + currentUser
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
